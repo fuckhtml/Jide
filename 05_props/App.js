f3f2466a4b3bba274c209d309fc199e43ea10014
Александр Григорьев <jordenblacksnk@gmail.com>
2020-08-11 15:25:58 +0300

    Other types of Props

--- 05_props/App.js
+++ 05_props/App.js
@@ -2,8 +2,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello to="Jide" from="Alex"/>
-        <Hello to="Anna" from="Alex's father"/>
+        <Hello
+          bool = {true}
+          num = {5}
+          str = {"Jide"}
+          list = {[1,2,3,4,5]}
+          obj = { {a: 1, b: 2, c: 3,} }
+          img = { "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
+        />
       </div>
     )
   }
--- 05_props/Hello.js
+++ 05_props/Hello.js
@@ -1,14 +1,28 @@
 class Hello extends React.Component {
   render() {
+    console.log(typeof this.props);
     console.log(this.props);
 
-    const to = this.props.to;
-    const from = this.props.from;
+    const bool = this.props.bool;
+    const num = this.props.num;
+    const str = this.props.str;
+    const list = this.props.list;
+    const obj = this.props.obj;
+    const img = this.props.img;
 
-    //this.props.to = "Ned Stark";
+    console.log(bool);
+    console.log(num);
+    console.log(str);
+    console.log(list);
+    console.log(obj);
+    console.log(img);
 
     return (
-      <h1>Hello to {to} from {from}</h1>
+      <div>
+        <h1>Data Types { "!".repeat(num) }</h1>
+        <p>Look at what is happening in the console</p>
+        <img src={img} />
+      </div>
     )
   }
 }
